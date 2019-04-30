bc7e458a4ecefbb493dde5892f1b4b208631289c
Roman Yakovenko <erelus@gmail.com>
2019-04-30 15:32:56 +0300

    install php zip extension

--- docker/app/provision.sh
+++ docker/app/provision.sh
@@ -3,10 +3,10 @@
 set -e
 
 apt-get -y -qq update 
-apt-get -y -qq install  git unzip libpng-dev libjpeg-dev libfreetype6-dev libicu-dev libfontconfig1 libxrender1 libxtst6 --no-install-recommends
+apt-get -y -qq install  git unzip libpng-dev libjpeg-dev libfreetype6-dev libicu-dev libfontconfig1 libxrender1 libxtst6 libzip-dev --no-install-recommends
 
 docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
-docker-php-ext-install -j$(nproc) pdo_mysql bcmath gd intl opcache
+docker-php-ext-install -j$(nproc) pdo_mysql bcmath gd intl opcache zip
 curl -Lo /usr/bin/composer https://getcomposer.org/composer.phar
 chmod +x /usr/bin/composer
 
